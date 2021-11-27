77dc1d170a725e11ddb52242bdb167c2359b492c
hari66mech <harikrishna.manokaran@atmecs.com>
2021-11-27 12:35:25 +0530

    login with standard_user credentials commit

--- constants/constant.py
+++ constants/constant.py
@@ -6,5 +6,6 @@ class Constant:
     USER_HOME = "C:" + os.path.sep + "Users" + os.path.sep + "harikrishna.manokara" + os.path.sep + "PycharmProjects" + os.path.sep + "sauce_demo" + os.path.sep
     DRIVER_PATH = USER_HOME + 'driver' + os.path.sep + 'chromedriver.exe'
     LOGIN_PAGE_URL = "https://www.saucedemo.com/"
+    HOME_PAGE_URL = "https://www.saucedemo.com/inventory.html"
     STANDARD_USER = "standard_user"
     PASSWORD = "secret_sauce"
