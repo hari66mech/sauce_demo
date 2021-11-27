a69a82b91e12a746c4c2e260f83f6d6483822d70
hari66mech <harikrishna.manokaran@atmecs.com>
2021-11-27 12:33:55 +0530

    login with standard_user credentials commit

--- constants/constant.py
+++ constants/constant.py
@@ -6,9 +6,5 @@ class Constant:
     USER_HOME = "C:" + os.path.sep + "Users" + os.path.sep + "harikrishna.manokara" + os.path.sep + "PycharmProjects" + os.path.sep + "sauce_demo" + os.path.sep
     DRIVER_PATH = USER_HOME + 'driver' + os.path.sep + 'chromedriver.exe'
     LOGIN_PAGE_URL = "https://www.saucedemo.com/"
-    HOME_PAGE_URL = "https://www.saucedemo.com/inventory.html"
-    CART_PAGE_URL = "https://www.saucedemo.com/cart.html"
-    CART_PAGE_TITLE = "Swag Labs"
-    ERROR_TEXT = "Epic sadface: Sorry, this user has been locked out."
     STANDARD_USER = "standard_user"
     PASSWORD = "secret_sauce"
